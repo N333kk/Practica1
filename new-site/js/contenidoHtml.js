d2ad24b59e7bb2d1aec0ad1948a0b2144cca7ad7
Nekk <pepechafer@gmail.com>
2024-10-11 16:55:26 +0200

    Pepe Chafer - Mejoras seccion contenidoHtml
    Realizadas mejoras de claridad en la seccion contenidoHtml

--- new-site/js/contenidoHtml.js
+++ new-site/js/contenidoHtml.js
@@ -2,5 +2,5 @@ const botonPrueba=document.getElementById('botonPrueba');
 let textoAModificar= document.getElementById('textoModificar');
 
 botonPrueba.addEventListener('click',event=>{
-    textoAModificar.innerHTML="Hola JavaScript!";
+    textoAModificar.innerHTML="Nuestro boton se encarga de modificar este texto al hacer click en el!";
 })
